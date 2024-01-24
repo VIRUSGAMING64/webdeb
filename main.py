9104d8f582af200d9029fe7567a2e24870763841
VIRUSGAMING64 <virusroger6@gmail.com>
2024-01-23 21:52:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import fastapi
 from pydantic import BaseModel
 app = fastapi.FastAPI()
-
-class nodo(BaseModel):
-    padre:int = 0
-    hijos:list = []
-
 @app.get("/")
 async def root():
     return {"hola":"mundo"}
